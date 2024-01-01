296a2773980f13958384c9f74f2531b869c94e08
aeshapatel025 <aesha.patel@spreadd.io>
2024-01-01 10:01:49 +0530

    add routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -6,10 +7,25 @@ import Contact from './components/Contact';
 const App = () => {
   return (
     <div>
-      App
-      <Home />
-      <About />
-      <Contact />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 };
